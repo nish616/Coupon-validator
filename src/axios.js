6dc7e93bdab93e88e083a672c808bd45fed25588
nish616 <nishin616@gmail.com>
2021-02-15 02:20:43 +0530

    fix baseURL

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    //baseURL : "https://sleepy-hamlet-70742.herokuapp.com"
-    baseURL : "http://localhost:5000"
+    baseURL : "https://sleepy-hamlet-70742.herokuapp.com"
+    //baseURL : "http://localhost:5000"
 });
 
 export default instance;      
